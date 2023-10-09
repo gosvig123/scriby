476bb32f6eb8928370eb567e5fd0fecb17769991
gosvig123 <109414278+gosvig123@users.noreply.github.com>
2023-10-09 09:35:54 +0200

    initial stages of reverting to whisper

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,10 +1,6 @@
 import handleFileUpload from '../../../lib/jwt/multerStorage';
 import { IAudioMetadata } from 'music-metadata';
-
-import {
-  transcribeAudioToBuffer,
-  parseAudioMetadata,
-} from '../../../lib/handleFileTranscription';
+import { parseAudioMetadata } from '../../../lib/handleFileTranscription';
 import authenticateUser from '../../../lib/jwt/authenticateUser';
 export const config = {
   api: {
@@ -16,6 +12,9 @@ import {
   createTranscriptionRecord,
   uploadToSupabase,
 } from '../../../lib/uploadFile';
+import { OPEN_AI_KEY } from '../../../constants';
+import FormData from 'form-data';
+import axios from 'axios';
 
 export default async (req: any, res: any) => {
   const userDetails = await authenticateUser(req);
@@ -31,14 +30,50 @@ export default async (req: any, res: any) => {
     if (!req.file || !req.file.buffer) {
       return res.status(400).json({ error: 'No file uploaded' });
     }
+    const originalName = req.file.originalname;
     const metadata: IAudioMetadata = await parseAudioMetadata(
       req.file.buffer
     );
-    const transcriptionBuffer = await transcribeAudioToBuffer(
-      req.file.buffer
+    // const transcriptionBuffer = await transcribeAudioToBuffer(
+    //   req.file.buffer
+    // );
+    const key = await OPEN_AI_KEY;
+    console.log('key', key);
+    const model = 'whisper-1';
+    const form = new FormData();
+    form.append('model', model);
+    form.append('file', req.file.buffer, {
+      filename: req.file.originalname,
+    });
+    console.log('buffer', req.file.buffer);
+    const response: any = await axios.post(
+      'https://api.openai.com/v1/audio/transcriptions',
+      {
+        headers: {
+          Authorization: `Bearer ${key}`,
+          ...form.getHeaders(),
+        },
+      }
     );
 
-    const originalName = req.file.originalname;
+    const transcription = response.data;
+    if (!transcription) {
+      console.error(
+        'No transcription found in the response:',
+        response
+      );
+      return res
+        .status(500)
+        .json({ error: 'No transcription received' });
+    }
+    console.log('transcription', transcription);
+
+    // Convert the transcribed text to a Buffer
+    const transcriptionBuffer = Buffer.from(
+      JSON.stringify({ transcription: transcription }),
+      'utf-8'
+    );
+
     await uploadToSupabase(email, originalName, transcriptionBuffer);
 
     await createTranscriptionRecord(originalName, id, metadata);
@@ -53,3 +88,48 @@ export default async (req: any, res: any) => {
       .json({ error: 'Error transcribing the audio' });
   }
 };
+
+
+    // // Transcription process
+    // const key = await OPEN_AI_KEY;
+    // const model = 'whisper-1';
+    // const formData = new FormData();
+    // formData.append('model', model);
+    // formData.append('file', req.file.buffer, {
+    //   filename: req.file.originalname,
+    // });
+
+    // try {
+    //   const response = await axios.post(
+    //     'https://api.openai.com/v1/audio/transcriptions',
+    //     formData,
+    //     {
+    //       headers: {
+    //         Authorization: `Bearer ${key}`,
+    //         ...formData.getHeaders(),
+    //       },
+    //     }
+    //   );
+
+    //   // Check if the response contains 'text'
+    //   const transcription = response.data.text;
+    //   if (!transcription) {
+    //     console.error(
+    //       'No transcription found in the response:',
+    //       response.data
+    //     );
+    //     return res
+    //       .status(500)
+    //       .json({ error: 'No transcription received' });
+    //   }
+
+    //   // Convert the transcribed text to a Buffer
+    //   const transcriptionBuffer = Buffer.from(
+    //     JSON.stringify({ transcription: transcription }),
+    //     'utf-8'
+    //   );
+
+    //   // Define the path for the .txt file in Supabase storage
+    //   const filePath = `${email}/${
+    //     req.file.originalname.split('.')[0]
+    //   }.json`;
