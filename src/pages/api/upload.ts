856b757d5b2500ff058cf77b4e35f129d7f06367
gosvig123 <109414278+gosvig123@users.noreply.github.com>
2023-11-27 16:16:11 +0100

    Feat, added deepgram database upload

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -2,8 +2,10 @@
 import multer from "multer";
 import { Deepgram } from "@deepgram/sdk";
 import { DEEPGRAM_API_KEY } from "../../../constants";
+import { authenticateUser } from "../../../lib/jwt/authenticateUser";
+import { prisma } from "../../../prisma/db";
 
-// Multer setup (for in-memory storage, can be adjusted as needed)
+// Multer setup (for in-memory storage)
 const upload = multer({ storage: multer.memoryStorage() });
 
 export const config = {
@@ -11,36 +13,87 @@ export const config = {
     bodyParser: false,
   },
 };
-export default async function handler(req: any, res: any) {
-  if (req.method !== "POST") {
-    return res.status(405).send("Method Not Allowed");
-  }
 
-  upload.single("file")(req, res, async (err) => {
-    if (err) {
-      return res.status(500).json({ error: "Error uploading the file" });
+// Promisify multer middleware
+const multerSingle = (fieldName: string) => {
+  return (req: any, res: any) => {
+    return new Promise((resolve, reject) => {
+      upload.single(fieldName)(req, res, (err: any) => {
+        if (err) {
+          reject(err);
+        } else if (req.file) {
+          resolve(req.file);
+        } else {
+          reject(new Error("No file uploaded"));
+        }
+      });
+    });
+  };
+};
+
+export default async function handler(req: any, res: any) {
+  try {
+    // Authenticate user
+    const userDetails = await authenticateUser(req);
+    const userId: any = userDetails.userId;
+
+    // Check for POST request
+    if (req.method !== "POST") {
+      return res.status(405).send("Method Not Allowed");
     }
 
-    const deepgram = new Deepgram(await DEEPGRAM_API_KEY);
+    // Process file upload
+    await multerSingle("file")(req, res);
 
+    // Check if file is uploaded
     if (!req.file) {
       return res.status(400).json({ error: "No file uploaded" });
     }
 
-    try {
-      const audio = req.file.buffer;
-      const source = { buffer: audio, mimetype: req.file.mimetype };
-      const response = await deepgram.transcription.preRecorded(source, {
-        smart_format: true,
-        model: "nova",
-      });
+    // Setup Deepgram
+    const deepgram = new Deepgram(await DEEPGRAM_API_KEY);
+    const audio = req.file.buffer;
+    const name = req.file.originalname;
+    const source = { buffer: audio, mimetype: req.file.mimetype };
 
-      console.log(response.results?.channels[0].alternatives[0]);
+    // Transcription with Deepgram
+    const response: any = await deepgram.transcription.preRecorded(source, {
+      smart_format: true,
+      model: "nova",
+    });
 
-      res.status(200).json(response);
-    } catch (error) {
-      console.error("Transcription Error:", error);
-      res.status(500).json({ error: "Error transcribing the audio" });
-    }
-  });
+    const transcriptionData = response.results?.channels[0].alternatives[0];
+
+    console.log("Transcription: 0", transcriptionData);
+    const fileTranscript =
+      transcriptionData.transcript || transcriptionData.Transcript;
+
+    const Fileconfidence = parseFloat(transcriptionData.confidence);
+    console.log("Transcript with conficence:", Fileconfidence);
+
+    const Filewords = transcriptionData.words;
+    const fileParagraps = transcriptionData.paragraphs;
+
+    // Creating new transcription in database
+    const newTranscription = await prisma.transcription.create({
+      data: {
+        name,
+        user: {
+          connect: {
+            id: userId, // Replace 'id' with the actual unique identifier field for User
+          },
+        },
+        confidence: Fileconfidence,
+        words: Filewords,
+        paragraphs: fileParagraps,
+        transcript: fileTranscript,
+      },
+    });
+
+    console.log(newTranscription);
+    res.status(200).json(newTranscription);
+  } catch (error) {
+    console.error("Transcription Error:", error);
+    res.status(500).json({ error: "Error transcribing the audio" });
+  }
 }
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,41 +1,34 @@
 // import postUser from '../serverActions/dbActions/postUser';
-import { FormEvent, useState, useEffect } from 'react';
+import { FormEvent, useState, useEffect } from "react";
 // import sendEmail from '../serverActions/sendEmail';
-import dynamic from 'next/dynamic';
-const GoogleLogin = dynamic(
-  () => import('../components/auth/GoogleLogin'),
-  {
-    ssr: false,
-  }
-);
-import Notification from '@/components/Alert';
-
-import { AuthLink } from '@prisma/client';
+import Notification from "../components/Alert";
+import dynamic from "next/dynamic";
+const GoogleLogin = dynamic(() => import("../components/auth/GoogleLogin"), {
+  ssr: false,
+});
 
 let gapi: any;
 const clientId =
-  '358155175620-tmo0ped23qte9gpnv4dovqr1i6tj11r6.apps.googleusercontent.com';
+  "358155175620-tmo0ped23qte9gpnv4dovqr1i6tj11r6.apps.googleusercontent.com";
 
 function Signup() {
-  type NotificationStatus = 'success' | 'failure' | 'info';
+  type NotificationStatus = "success" | "failure" | "info";
 
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
   const [isChecked, setIsChecked] = useState(true);
-  const [statusState, setStatusState] =
-    useState<NotificationStatus>('info');
-  const [notificationTextState, setNotificationTextState] =
-    useState('info');
+  const [statusState, setStatusState] = useState<NotificationStatus>("info");
+  const [notificationTextState, setNotificationTextState] = useState("info");
   const [authNotification, setAuthNotification] = useState(false);
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      import('gapi-script').then((module) => {
+    if (typeof window !== "undefined") {
+      import("gapi-script").then((module) => {
         gapi = module.gapi;
-        gapi.load('client:auth2', () => {
+        gapi.load("client:auth2", () => {
           gapi.client.init({
             clientId: clientId,
-            scope: '',
+            scope: "https://www.googleapis.com/auth/userinfo.profile",
           });
         });
       });
@@ -50,115 +43,141 @@ function Signup() {
 
   async function handleSignup(e: FormEvent) {
     e.preventDefault();
-
     console.log(isChecked);
     if (!isChecked) {
-      console.log('not checked');
-      setStatusState('info');
+      console.log("not checked");
+      setStatusState("info");
       setNotificationTextState(
-        'Please make sure to accept our terms of service'
+        "Please make sure to accept our terms of service"
       );
       setAuthNotification(true);
       await handleNotificationClose();
       return;
     }
 
-    const response = await fetch('/api/auth/signup', {
-      method: 'POST',
+    const response = await fetch("/api/auth/signup", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ email, password }),
     });
 
     const data = await response.json();
 
+    console.log("data", data);
     if (response.status === 200) {
       console.log(data);
-      setStatusState('success');
-      setNotificationTextState('You have successfully signed up!');
+      setStatusState("success");
+      setNotificationTextState("You have successfully signed up!");
       setAuthNotification(true);
       await handleNotificationClose();
     } else {
-      setStatusState('failure');
+      setStatusState("failure");
       setNotificationTextState(data.message);
       setAuthNotification(true);
       await handleNotificationClose();
     }
   }
 
+  const handleGoogleLoginSuccess = async (response: any) => {
+    console.log(response);
+    // The response object has a different structure in react-google-login
+    const { googleId, profileObj } = response;
+    const { email, name, imageUrl } = profileObj;
+
+    // Now, you can use this information as per your app's requirements
+    // For example, send this data to your backend server for user creation or verification
+    try {
+      const userResponse = await fetch("/api/auth/google-login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          googleId,
+          name,
+          email,
+          imageUrl,
+        }),
+      });
+
+      const data = await userResponse.json();
+
+      if (userResponse.ok) {
+        // Handle successful user creation or login
+        console.log("User logged in:", data);
+        // Redirect to dashboard or other page
+        // window.location.href = '/dashboard';
+      } else {
+        // Handle errors
+        console.error("Login error:", data.message);
+      }
+    } catch (error) {
+      console.error("Request failed", error);
+    }
+  };
+
   return (
-    <div className='h-screen w-full flex flex-col justify-center items-center'>
-      <div className='w-4/6 h-4/5 bg-white text-center items-center px-32 py-10 flex flex-col justify-around rounded-lg shadow-lg'>
-        <div className='flex flex-col w-full'>
-          <h1 className='text-3xl font-sans font-bold'>
-            Become a member
-          </h1>
-          <p className='text-lg text-gray-800 mt-3'>
-            Sign up with Google or use the form to create a new
-            account.
+    <div className="h-screen w-full flex flex-col justify-center items-center">
+      <div className="w-4/6 h-4/5 bg-white text-center items-center px-32 py-10 flex flex-col justify-around rounded-lg shadow-lg">
+        <div className="flex flex-col w-full">
+          <h1 className="text-3xl font-sans font-bold">Become a member</h1>
+          <p className="text-lg text-gray-800 mt-3">
+            Sign up with Google or use the form to create a new account.
           </p>
         </div>
 
-        <GoogleLogin />
-        <div className='w-full flex flex-col'>
+        <GoogleLogin onSuccess={handleGoogleLoginSuccess} />
+        <div className="w-full flex flex-col">
           <form
-            className='flex flex-col gap-5 text-left text-lg'
+            className="flex flex-col gap-5 text-left text-lg"
             onSubmit={handleSignup}
           >
             <label>
               Email
               <input
-                type='email'
+                type="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                placeholder='Enter your email'
-                className='mt-1 w-full bg-[#ECECECFF] p-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-500'
+                placeholder="Enter your email"
+                className="mt-1 w-full bg-[#ECECECFF] p-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-500"
               />
             </label>
             <label>
               Password
               <input
-                type='password'
+                type="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                placeholder='Enter your password'
-                className='mt-1 w-full bg-[#ECECECFF] p-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-500'
+                placeholder="Enter your password"
+                className="mt-1 w-full bg-[#ECECECFF] p-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-500"
               />
             </label>
-            <div className='flex items-center'>
+            <div className="flex items-center">
               <input
-                type='checkbox'
+                type="checkbox"
                 checked={isChecked}
                 onChange={() => setIsChecked(!isChecked)}
-                className='mr-2 h-5 w-5 text-purple-500 border-gray-300 rounded focus:ring-2 focus:ring-purple-500'
+                className="mr-2 h-5 w-5 text-purple-500 border-gray-300 rounded focus:ring-2 focus:ring-purple-500"
               />
 
-              <label htmlFor='terms'>
-                By signing up, you accept our terms of service and
-                privacy policy.
+              <label htmlFor="terms">
+                By signing up, you accept our terms of service and privacy
+                policy.
               </label>
             </div>
-            <button
-              className='w-full solidPurpleButton h-16'
-              type='submit'
-            >
+            <button className="w-full solidPurpleButton h-16" type="submit">
               Sign up
             </button>
           </form>
         </div>
       </div>
       {authNotification && (
-        <Notification
-          text={notificationTextState}
-          status={statusState}
-        />
+        <Notification text={notificationTextState} status={statusState} />
       )}
     </div>
   );
 }
 
-function isAuthLink(obj: any): obj is AuthLink {
-  return obj && obj.userId && typeof obj.userId === 'number';
-}
 export default Signup;
